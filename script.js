fb2ace90d6a0c385ccba209c3a139f3f0a72b18f
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-12-07 12:59:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-//Get all required elements
-const form = document.querySelector('form'),
-statusTxt = form.querySelector('.button-area span');
-
+//Contact Form in PHP
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e)=>{
-  e.preventDefault()// prevent form from submitting 
+  e.preventDefault(); //preventing form from submitting 
+  statusTxt.style.color = "#0D6EFD";
   statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
 
   let xhr = new XMLHttpRequest(); //creating new xml object
-
   xhr.open("POST", "message.php", true); //sending POST request to message.php file
-  xhr.onload = ()=> { // once ajax's loaded
-    if(xhr.readyState == 4 && xhr.status == 200) { //if ajax response status is 200 & ready status is 4 = there is no error
-      let response = xhr.response; //storing ajax response in a response variable
-      console.log(response);
+  xhr.onload = ()=>{ //once ajax loaded 
+    if(xhr.readyState == 4 && xhr.status == 200){ //if ajax response status is 200 & ready is 4 = there is no any error
+      let response = xhr.response; //storing ajax response in a response variable 
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
     }
   }
-  
-  xhr.send();
+  let formData = new FormData(form); //creating new FormData obj. This is used to send form data
+  xhr.send(formData); //sending form data
 }
